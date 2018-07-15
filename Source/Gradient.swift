43364a4fe8fddcd39f1d9280bd0c73b9f4cf0ae5
Adam <codeswift@icloud.com>
2018-07-15 14:20:31 -0500

    Moving gradient points and angle to Gradient

--- Source/Gradient.swift
+++ Source/Gradient.swift
@@ -1,15 +1,54 @@
 import UIKit
 
-public struct GradientComponents {
-
-    let color: UIColor
-    let location: Double
-
-}
-
 public protocol Gradient {
 
     var angle: GradientAngle { get }
     func makeGradientComponents() -> [GradientComponents]
 
+}
+
+public struct GradientPoints {
+    
+    public var start: CGPoint
+    public var end: CGPoint
+    
+}
+
+public struct GradientComponents {
+    
+    let color: UIColor
+    let location: Double
+    
+}
+
+public enum GradientAngle {
+    
+    case custom(start: CGPoint, end: CGPoint)
+    case bottomLeftToTopRight
+    case topLeftToBottomRight
+    case horizontal
+    case vertical
+    
+    public static let defaultAngle = GradientAngle.vertical
+    
+    var gradientPoints: GradientPoints {
+        switch self {
+            
+        case .custom (let start, let end):
+            return GradientPoints(start: start, end: end)
+            
+        case .bottomLeftToTopRight:
+            return GradientPoints(start: CGPoint(x: 0.0, y: 1.0), end: CGPoint(x: 1.0, y: 0.0))
+            
+        case .topLeftToBottomRight:
+            return GradientPoints(start: CGPoint(x: 0.0, y: 0.0), end: CGPoint(x: 1.0, y: 1.0))
+            
+        case .horizontal:
+            return GradientPoints(start: CGPoint(x: 0.0, y: 0.5), end: CGPoint(x: 1.0, y: 0.5))
+            
+        case .vertical:
+            return GradientPoints(start: CGPoint(x: 0.5, y: 0.0), end: CGPoint(x: 0.5, y: 1.0))
+        }
+    }
+    
 }
--- Source/PercentageGradient.swift
+++ Source/PercentageGradient.swift
@@ -7,39 +7,6 @@ public enum GradientDirection {
 
 }
 
-public struct GradientPoints {
-    
-    public var start: CGPoint
-    public var end: CGPoint
-    
-}
-
-public enum GradientAngle {
-    
-    public static let defaultAngle = GradientAngle.vertical
-    var gradientPoints: GradientPoints {
-        switch self {
-        case .custom (let start, let end):
-            return GradientPoints(start: start, end: end)
-        case .bottomLeftToTopRight:
-            return GradientPoints(start: CGPoint(x: 0.0, y: 1.0), end: CGPoint(x: 1.0, y: 0.0))
-        case .topLeftToBottomRight:
-            return GradientPoints(start: CGPoint(x: 0.0, y: 0.0), end: CGPoint(x: 1.0, y: 1.0))
-        case .horizontal:
-            return GradientPoints(start: CGPoint(x: 0.0, y: 0.5), end: CGPoint(x: 1.0, y: 0.5))
-        case .vertical:
-            return GradientPoints(start: CGPoint(x: 0.5, y: 0.0), end: CGPoint(x: 0.5, y: 1.0))
-        }
-    }
-    
-    case custom(start: CGPoint, end: CGPoint)
-    case bottomLeftToTopRight
-    case topLeftToBottomRight
-    case horizontal
-    case vertical
-    
-}
-
 public struct PercentageGradient: Gradient {
 
     public let angle: GradientAngle
